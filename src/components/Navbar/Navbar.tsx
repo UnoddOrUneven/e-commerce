d1812de862850fb56944eb87b33720684c83026f
Roman <Romangorbunov4890@gmail.com>
2026-09-24 12:50:15 +0400

    add stubs for cart, username and catalogue

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,19 @@
 import "./navbar.css"
 export function Navbar() {
     return (
-        <aside className="left-bar">
+        <div className="left-bar">
 
             <div className="title"> Claire</div>
-        </aside>
+
+            <div>
+                Cart
+            </div>
+            <div>Catalogue</div>
+
+
+            <div className="profile">
+            Username
+            </div>
+        </div>
     )
 }
